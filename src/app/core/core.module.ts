921b92ff99594a54ac07dc7e33ce7f01fe3edb84
junichi-hatano <junichi-hatano@hands-lab.com>
2018-11-28 22:57:48 +0900

    状態管理のためのstore追加

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,6 +6,8 @@ import { FormsModule } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { StoreModule } from '@ngrx/store';
+import * as fromCore from './state';
 
 @NgModule({
   imports: [
@@ -14,7 +16,8 @@ import { MatIconModule } from '@angular/material/icon';
     FormsModule,
     MatCheckboxModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    StoreModule.forFeature('core', fromCore.reducers, { metaReducers: fromCore.metaReducers })
   ],
   declarations: [LayoutComponent],
   exports: [
